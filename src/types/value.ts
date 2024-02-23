cb21844346c1d44e5ab67c003386a1ce4e795b71
Marti Serra <martiserramolina99@gmail.com>
2024-02-23 11:15:47 +0100

    Added comments

--- src/types/value.ts
+++ src/types/value.ts
@@ -2,8 +2,17 @@ import { PromptObject } from 'prompts';
 
 import { Value } from 'mongu';
 
+/**
+ * It represents prompts.
+ */
 export type ValuePrompts = PromptObject;
 
+/**
+ * It represents what is returned.
+ */
 export type ValueReturn = Value;
 
+/**
+ * It represents variables. Each property corresponds to a variable.
+ */
 export type ValueVariables = { [key: string]: Value };
